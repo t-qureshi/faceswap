adcc012542986d3fbf0a7e6e88b82d78168679f5
t-qureshi <61450584+t-qureshi@users.noreply.github.com>
2020-10-21 10:44:12 +0500

    Add files via upload

---
+++ Requirments.sh
@@ -0,0 +1,7 @@
+sudo apt install python3.7
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3.7 get-pip.py
+python3.7 -m pip install -r requirements_nvidia.txt
+python3.7 -m pip install -r_requirements_base.txt
+python3.7 -m pip install face-recognition
+python3.7 setup.py
